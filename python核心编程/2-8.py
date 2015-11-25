dd396cfb6709a5a634d6e78436f061aba72d2e3d
RoseOu <598959770@qq.com>
2015-11-25 11:25:36 +0800

    Update 2-8.py

--- python核心编程/2-8.py
+++ python核心编程/2-8.py
@@ -11,8 +11,8 @@ print 'Total:', total
 
 #for
 total=0
-a=[1, 2, 3, 4, 5]
-for i in range(0, 5):
+a=[1,2,3,4,5]
+for i in range(0,5):
     print 'Input number',i+1
     a[i]=float(raw_input())
     total=total + a[i]
