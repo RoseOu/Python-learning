0717388cbc7c1bc8f62bba9bcb6ffcd17f545839
RoseOu <598959770@qq.com>
2016-03-24 22:10:52 +0800

    python

---
+++ python核心编程/chapter8/8-5.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+'''
+约数。完成一个名为getfactors()的函数。它接受一个整型作为参数，返回它所有约数的列表，包括1和它本身。
+'''
+
+num = int(input("Input the number here:"))
+fac_list = []
+def getfactors(num):
+	i = 1
+	while i <= num:
+		if num%i == 0:
+			fac_list.append(i)
+		i = i+1
+	return fac_list
+print getfactors(num)
